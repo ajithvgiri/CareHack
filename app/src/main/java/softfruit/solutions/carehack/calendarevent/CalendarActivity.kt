a0bc01193661cbbc28432d1345c0a085c86275a4
Shakeeb Mancheri <shakeebsoftfruit@gmail.com>
2017-11-15 22:14:43 +0530

    Merge branch 'master' of https://github.com/ajithvgiri/QuickDoc
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/softfruit/solutions/carehack/calendarevent/CalendarActivity.kt
+++ app/src/main/java/softfruit/solutions/carehack/calendarevent/CalendarActivity.kt
@@ -1,21 +1,10 @@
 package softfruit.solutions.carehack.calendarevent
 
-import android.app.usage.UsageEvents
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import softfruit.solutions.carehack.R
-
 import kotlinx.android.synthetic.main.activity_calendar.*
-import java.util.Arrays.asList
-import android.provider.CalendarContract.Reminders
-import jdk.nashorn.internal.objects.NativeDate.setMinutes
-import java.util.*
-import java.util.Arrays.asList
-import android.provider.CalendarContract.Reminders
-import jdk.nashorn.internal.objects.NativeDate.setMinutes
-
-
+import softfruit.solutions.carehack.R
 
 
 class CalendarActivity : AppCompatActivity() {
@@ -31,40 +20,40 @@ class CalendarActivity : AppCompatActivity() {
         }
     }
 
-    fun createEvent(mCredential: GoogleAccountCredential) {
+    fun createEvent() {
 
-        var event = Event()
-                .setSummary("Google I/O 2015")
-                .setLocation("800 Howard St., San Francisco, CA 94103")
-                .setDescription("A chance to hear more about Google's developer products.")
-
-        val startDateTime = DateTime("2015-05-28T09:00:00-07:00")
-        val start = EventDateTime()
-                .setDateTime(startDateTime)
-                .setTimeZone("America/Los_Angeles")
-        event.setStart(start)
-
-        val endDateTime = DateTime("2015-05-28T17:00:00-07:00")
-        val end = EventDateTime()
-                .setDateTime(endDateTime)
-                .setTimeZone("America/Los_Angeles")
-        event.setEnd(end)
-
-        val recurrence = arrayOf("RRULE:FREQ=DAILY;COUNT=2")
-        event.setRecurrence(Arrays.asList(recurrence))
-
-        val attendees = arrayOf<EventAttendee>(EventAttendee().setEmail("lpage@example.com"), EventAttendee().setEmail("sbrin@example.com"))
-        event.setAttendees(Arrays.asList(attendees))
-
-        val reminderOverrides = arrayOf<EventReminder>(EventReminder().setMethod("email").setMinutes(24 * 60), EventReminder().setMethod("popup").setMinutes(10))
-        val reminders = Event.Reminders()
-                .setUseDefault(false)
-                .setOverrides(Arrays.asList(reminderOverrides))
-        event.setReminders(reminders)
-
-        val calendarId = "primary"
-        event = service.events().insert(calendarId, event).execute()
-        System.out.printf("Event created: %s\n", event.getHtmlLink())
+//        var event = Event()
+//                .setSummary("Google I/O 2015")
+//                .setLocation("800 Howard St., San Francisco, CA 94103")
+//                .setDescription("A chance to hear more about Google's developer products.")
+//
+//        val startDateTime = DateTime("2015-05-28T09:00:00-07:00")
+//        val start = EventDateTime()
+//                .setDateTime(startDateTime)
+//                .setTimeZone("America/Los_Angeles")
+//        event.setStart(start)
+//
+//        val endDateTime = DateTime("2015-05-28T17:00:00-07:00")
+//        val end = EventDateTime()
+//                .setDateTime(endDateTime)
+//                .setTimeZone("America/Los_Angeles")
+//        event.setEnd(end)
+//
+//        val recurrence = arrayOf("RRULE:FREQ=DAILY;COUNT=2")
+//        event.setRecurrence(Arrays.asList(recurrence))
+//
+//        val attendees = arrayOf<EventAttendee>(EventAttendee().setEmail("lpage@example.com"), EventAttendee().setEmail("sbrin@example.com"))
+//        event.setAttendees(Arrays.asList(attendees))
+//
+//        val reminderOverrides = arrayOf<EventReminder>(EventReminder().setMethod("email").setMinutes(24 * 60), EventReminder().setMethod("popup").setMinutes(10))
+//        val reminders = Event.Reminders()
+//                .setUseDefault(false)
+//                .setOverrides(Arrays.asList(reminderOverrides))
+//        event.setReminders(reminders)
+//
+//        val calendarId = "primary"
+//        event = service.events().insert(calendarId, event).execute()
+//        System.out.printf("Event created: %s\n", event.getHtmlLink())
 
     }
 }
